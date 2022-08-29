4422bf249eb6c75722bac10dd2304e2b5ea1015f
J <18084450+JelmerDeHen@users.noreply.github.com>
2022-08-29 21:46:30 +0200

    Isolate mp3 playing logic to mp3.go

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	_ "bytes"
-	_ "io"
 	"log"
 	"time"
-
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
 
 	"github.com/JelmerDeHen/xidle"
 )
 
 var (
-	config  *Config
-	running bool
+	config *Config
 
 	// These bools guard executing afk() and present() handler multiple times
 	notifiedAfk     bool
@@ -42,31 +35,21 @@ func init() {
 	}
 
 	mp3player = &Mp3Player{
-		File: config.Mp3File(),
+		File:      config.Mp3File(),
+		HourStart: config.Mp3HourStart(),
+		HourStop:  config.Mp3HourStop(),
 	}
+	mp3player.Init()
 
-	// speaker.Init() should be called once
-	_, format, err := mp3.Decode(mp3player.GetMp3ReadCloser())
-	if err != nil {
-		log.Fatal(err)
-	}
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-	//
 	lastSeen = time.Now()
 	lastPresent = time.Now()
 }
 
 func afk() {
-	if running {
-		return
-	}
-	running = true
 	notifiedPresent = false
+	mp3player.Play()
 
-	playmp3()
 	if !notifiedAfk {
-
 		if config.I3lock() {
 			i3lock(config.I3lockColor())
 		}
@@ -92,52 +75,6 @@ func present() {
 		log.Printf("User is back: afkTime=%s\n", afkTime)
 		notifiedPresent = true
 	}
-}
-
-// Plays mp3 when certain conditions are met
-// The callback for speaker.Play is asynchronous so always set running=false when returning early
-func playmp3() {
-	now := time.Now()
-
-	// finishedPlayingTimestamp is updated after playing mp3
-	// durationSinceLastPlay describes duration since last time finishing playing mp3
-	// Check if this duration is greater than the config config.Mp3Interval()
-	durationSinceLastPlay := now.Sub(finishedPlayingTimestamp)
-	if durationSinceLastPlay < config.Mp3Interval() {
-		running = false
-		return
-	}
-
-	// config.Mp3HourStart() and config.Mp3HourStop() allow configuring the time between which to play mp3
-	start := config.Mp3HourStart()
-	if now.Hour() < start {
-		if !notifiedAfk {
-			log.Printf("playmp3(): Skip: only playing mp3 from %d o'clock\n", start)
-		}
-		running = false
-		return
-	}
-
-	stop := config.Mp3HourStop()
-	if now.Hour() != 0 && now.Hour() > stop {
-		if !notifiedAfk {
-			log.Printf("playmp3(): Skip: only playing mp3 until %d o'clock\n", stop)
-		}
-		running = false
-		return
-	}
-
-	// Play mp3 file
-	streamer, _, err := mp3.Decode(mp3player.GetMp3ReadCloser())
-	if err != nil {
-		log.Fatal(err)
-	}
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-		// This callback runs after streamer completed playing mp3
-		running = false
-		finishedPlayingTimestamp = time.Now()
-	})))
-	return
 }
 
 func main() {
--- mp3.go
+++ mp3.go
@@ -12,13 +12,17 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-/*
-running is neccessary because speaker should finish playing mp3 until next mp3 is played
-*/
-
 type Mp3Player struct {
-	File    string
-	Running bool
+	File      string
+	HourStart int
+	HourStop  int
+
+	playing bool
+
+	// finishedPlayingTimestamp is updated after playing mp3
+	// durationSinceLastPlay describes duration since last time finishing playing mp3
+	// Check if this duration is greater than the config config.Mp3Interval()
+	finishedPlayingTimestamp time.Time
 }
 
 func (m *Mp3Player) Init() {
@@ -41,51 +45,47 @@ func (m *Mp3Player) GetMp3ReadCloser() io.ReadCloser {
 	return rc
 }
 
-func (m *Mp3Player) OkToPlay() {
+// Plays mp3 when certain conditions are met
+// The callback for speaker.Play is asynchronous so always set m.playing=false when returning early
+func (m *Mp3Player) Play() {
 	now := time.Now()
 
-	if m.Running {
+	if m.playing {
 		return
 	}
 
-	// finishedPlayingTimestamp is updated after playing mp3
-	// durationSinceLastPlay describes duration since last time finishing playing mp3
-	// Check if this duration is greater than the config config.Mp3Interval()
-	durationSinceLastPlay := now.Sub(finishedPlayingTimestamp)
-	if durationSinceLastPlay < config.Mp3Interval() {
-		running = false
+	// Compare duration since last mp3 ended until now and compare it to the config.Mp3Interval()
+	if now.Sub(m.finishedPlayingTimestamp) < config.Mp3Interval() {
+		m.playing = false
 		return
 	}
 
-	// config.Mp3HourStart() and config.Mp3HourStop() allow configuring the time between which to play mp3
-	start := config.Mp3HourStart()
-	if now.Hour() < start {
+	// config.Mp3HourStart() and config.Mp3HourStop() allow configuring the hours of day between which to play mp3
+	if now.Hour() < m.HourStart {
 		if !notifiedAfk {
-			log.Printf("playmp3(): Skip: only playing mp3 from %d o'clock\n", start)
+			log.Printf("playmp3(): Skip: only playing mp3 from %d o'clock\n", m.HourStart)
 		}
-		running = false
+		m.playing = false
 		return
 	}
 
-	stop := config.Mp3HourStop()
-	if now.Hour() != 0 && now.Hour() > stop {
+	if now.Hour() != 0 && now.Hour() > m.HourStop {
 		if !notifiedAfk {
-			log.Printf("playmp3(): Skip: only playing mp3 until %d o'clock\n", stop)
+			log.Printf("playmp3(): Skip: only playing mp3 until %d o'clock\n", m.HourStop)
 		}
-		running = false
+		m.playing = false
 		return
 	}
-}
 
-func (m *Mp3Player) Play() {
-	// Play mp3 file
+	// All good - play mp3
+	m.playing = true
 	streamer, _, err := mp3.Decode(m.GetMp3ReadCloser())
 	if err != nil {
 		log.Fatal(err)
 	}
 	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
 		// This callback runs after streamer completed playing mp3
-		running = false
-		finishedPlayingTimestamp = time.Now()
+		m.playing = false
+		m.finishedPlayingTimestamp = time.Now()
 	})))
 }
